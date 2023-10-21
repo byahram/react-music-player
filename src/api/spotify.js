e00a79f47ba10c6df4ffc40bd58c985beebbd7cd
Ahram Kim <ahram0223@gmail.com>
2023-10-22 01:09:57 +0900

    Music Player #4: Integrating the Spotify Api

---
+++ src/api/spotify.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = 'a436f90d446b43eb9c4a4408063baa4d';
+const redirectUri = "http://localhost:3000";
+const scopes = ["user-read-private", "user-read-email"];
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(async function(config) {
+    config.headers.Authorization = `Bearer ` + token;
+    return config;
+  });
+};
+
+export default apiClient;
